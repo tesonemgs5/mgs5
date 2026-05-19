6713d290aa8bb83a645f8315464f55c7c22cd97b
tesonemgs5 <elettricotesone@gmail.com>
2026-05-19 15:24:33 +0200

    fix 3

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         background_color: '#0a0f1e',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
+        start_url: '/mgs5-app/',
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
