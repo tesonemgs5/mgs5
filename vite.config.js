93ddd020e486074d8d1b9849c20151528ee2ef0e
tesonemgs5 <elettricotesone@gmail.com>
2026-05-19 15:29:14 +0200

    fix3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: './',   // necessario per GitHub Pages
+  base: '/mgs5-app/',
   build: {
     outDir: 'docs'
   },
